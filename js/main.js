8d202cfcb59287808de38c74fc27876f7067e68b
NagypalMarton <nagypalmarton@outlook.com>
2022-05-29 14:03:40 +0200

    JavaScript-tel bíővítve és az input mezőket kötelezően ki kell tölteni!

--- js/main.js
+++ js/main.js
@@ -10,13 +10,37 @@ function ordersTextChance() {
     let amountInput=document.querySelector("input[name='amount-input']"); /*Ezzel lehet kiválasztani, hogy melyik elemet */
     let showAmount=document.querySelector("span.show-amount-kedvnel")
     let showDarab=document.querySelector("span.show-mennyiseg")
-    let amouant1=parseInt(amountInput.value)*testPrice
-    showAmount.innerHTML=amouant1+" Ft"
-    showDarab.innerHTML= showDarab+" db"
+    let amouantNumber=isNaN(parseInt(amountInput.value))? 1:alert('Nem megfelelő érték!') /*Három operandusú feltétel*/
+    
+    if(isNaN(amouantNumber)){/*Ellenőrzi, hogy üres-e */
+        amouantNumber=1
+    }else if(typeof amouantNumber != Number){
+        alert('Érvénytelen érték! Új értéke: 1 db')
+        amouantNumber=1
+    }
 
-    let kedvInput=document.querySelector("input[type=radio]:checked")
+    if(amouantNumber>10){
+        alert("Max 10 terméket vásárolhatsz meg!")
+        return
+    }else if(amouantNumber<1){
+        alert("Min 1 terméket vásárolhatsz meg!")
+    }
+    
+    let amouant1=amouantNumber*testPrice
+    showAmount.innerHTML=amouant1+" Ft"
+    showDarab.innerHTML= amouantNumber+" db"
+
+    let kedvInput=document.querySelector("input[type=radio]")
     let showKedvNev=document.querySelector("span.show-amount-kedvneve")
     showKedvNev.innerHTML=kedvInput.value
 
+    let kedvSzaz=0.15
     
+    let showKedv=document.querySelector('span.show-amount-kedverteke')
+    showKedv.innerHTML=(kedvSzaz*100).toPrecision(2)+"%, melynek értéke: "+(amouant1*kedvSzaz).toFixed(0)+" Ft"
+    
+    let showAmountEnd=document.querySelector('span.show-amount-kedv')
+    let amountEnd=amouant1-(amouant1*kedvSzaz).toFixed(0)
+    showAmountEnd.innerHTML=amountEnd+" Ft"
+    alert('Ez egy demo űrlap, amely sehova se küldi el és menti el a bevitt adatokat! Ellenőrzés egyenlőre csak a Mennyiség megadásánál van!')
 }
