a8b6e3afb729020e1bc88de292cc78e1ad051267
NagypalMarton <nagypalmarton@outlook.com>
2022-05-29 03:41:27 +0200

    FélKész Forms-os JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("Hello, This is the console Message!");
+function calcAmount() {
+    let testPrice=1200
+    let amountInput=document.querySelector("input[name='amount-input']"); /*Ezzel lehet kiválasztani, hogy melyik elemet */
+    let amouant=parseInt(amountInput.value)*testPrice
+    alert("Fizetendő: "+amouant+" Ft")
+}
+
+function ordersTextChance() {
+    let testPrice=1200
+    let amountInput=document.querySelector("input[name='amount-input']"); /*Ezzel lehet kiválasztani, hogy melyik elemet */
+    let showAmount=document.querySelector("span.show-amount-kedvnel")
+    let showDarab=document.querySelector("span.show-mennyiseg")
+    let amouant1=parseInt(amountInput.value)*testPrice
+    showAmount.innerHTML=amouant1+" Ft"
+    showDarab.innerHTML= showDarab+" db"
+
+    let kedvInput=document.querySelector("input[type=radio]")
+    let showKedvNev=document.querySelector("span.show-amount-kedvneve")
+    showKedvNev.innerHTML=kedvInput.value
+
+    
+}
