1f5bc7dac8f7a8c6d70994ea2e91e3dbc0ceaceb
NagypalMarton <nagypalmarton@outlook.com>
2022-06-05 17:48:34 +0200

    9 - F2 - 3/4 -> Csak az első gomb esetén müködik

--- js/adminbt_int.js
+++ js/adminbt_int.js
@@ -5,13 +5,6 @@ for (let i = 0; i < mytd.length; i++) {
     mytd[i].setAttribute("class", `td_sor${line}`);
     if (i == mod_osz) {
         mytd[i].setAttribute("class", `mod_gomb${line}`);
-
-        let submit_mod_att=document.querySelector('i').getAttribute('class')
-        if(submit_mod_att=='.fa-solid fa-pen-nib'){
-            mytd[i].setAttribute("type","submit_mod")/*Módosító gomb */
-        }else{
-            mytd[i].setAttribute("type","submit_del")/*Törlő gomb */
-        }
         mod_osz += 5
         line += 1
     }
@@ -24,10 +17,19 @@ for (let i = 0; i < mytr.length; i++) {
 }
 
 let btn_modi = document.querySelector("button[type='submit_mod']") /*Módosítás GOMB */
-let btn_dele = document.querySelector("button[type='submit_del'")
+let btn_dele = document.querySelector("button[type='submit_del']") /*Törlő GOMB */
+let btn_ment = document.querySelector("button[type='submit_ment']") /*Mentés GOMB */
+
+
+function choiceLine(line) {/*Aktuális sor sorszámát választja ki */
+    let sorScope = document.querySelector('th[scope="row"]').firstChild.nodeValue
+    alert(sorScope)
+    return sorScope
+}
 
 btn_modi.onclick = function () {
     let addInput = document.createElement('input');
+    line = choiceLine(line)
     addInput.className = "form-control td_sor"
     addInput.ariaRoleDescription = "inputGroup-sizing-lg"
     let myNodeList = document.querySelectorAll(`.td_sor${line}`)
@@ -35,13 +37,28 @@ btn_modi.onclick = function () {
         addInput.defaultValue = myNodeList[i].firstChild.nodeValue
         myNodeList[i].appendChild(addInput.cloneNode(true))
     }
+    document.querySelector('button[type="submit_mod"]').setAttribute('type','submit_ment')
 }
 btn_dele.onclick = function () {
+    line = choiceLine(line)
     const element = document.querySelector(`#C${line}`)
     if (confirm("Biztos törölni akarod a rekordod?")) {
         element.remove()
         alert('Törlésre került!')
-    }else{
+    } else {
         console.log('Siker!')
     }
 }
+
+let table_row=document.querySelector(".table_row")
+table_row.addEventListener("submit_ment",function(ev){/*íGY MÁR el is küldhetnénk az adatokat */
+    ev.preventDefault();
+    /*console.log(this);*/
+
+    let inputs=this.querySelectorAll("input");
+    let values={}
+    for (let i = 0; i < inputs.length; i++) {
+         values[inputs[i].name]=inputs.value
+    }
+    console.log(values);
+});
