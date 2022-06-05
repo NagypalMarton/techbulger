31bf0d9f49e7a56bbc42aac9797aa331428def39
NagypalMarton <nagypalmarton@outlook.com>
2022-06-05 16:43:30 +0200

    9 - F2 - HALF

--- js/adminbt_int.js
+++ js/adminbt_int.js
@@ -1,27 +1,47 @@
 let mytd = document.querySelectorAll("td")
-let j=4
+let mytr = document.querySelectorAll("tr")
+let mod_osz = 4, line = 1
 for (let i = 0; i < mytd.length; i++) {
-    mytd[i].setAttribute("class", "td_sor");
-    if (i == j) {
-        mytd[i].setAttribute("class", "mod_gomb");
-        j+=5
+    mytd[i].setAttribute("class", `td_sor${line}`);
+    if (i == mod_osz) {
+        mytd[i].setAttribute("class", `mod_gomb${line}`);
+
+        let submit_mod_att=document.querySelector('i').getAttribute('class')
+        if(submit_mod_att=='.fa-solid fa-pen-nib'){
+            mytd[i].setAttribute("type","submit_mod")/*Módosító gomb */
+        }else{
+            mytd[i].setAttribute("type","submit_del")/*Törlő gomb */
+        }
+        mod_osz += 5
+        line += 1
+    }
+    if (i + 1 == mytd.length) {
+        line = 1
     }
 }
+for (let i = 0; i < mytr.length; i++) {
+    mytr[i].setAttribute("id", `C${i}`);
+}
 
-let TableForm = document.querySelector(".table-group-divider")
 let btn_modi = document.querySelector("button[type='submit_mod']") /*Módosítás GOMB */
 let btn_dele = document.querySelector("button[type='submit_del'")
+
 btn_modi.onclick = function () {
-    alert('Teszt Szöveg')
-    let addInput = document.createElement("input");
-    addInput.className = "form-control"
-    let parents = document.querySelector("td")
-    parents.appendChild(addInput)
-    console.log(document.querySelector(".table_row").attributes)
+    let addInput = document.createElement('input');
+    addInput.className = "form-control td_sor"
+    addInput.ariaRoleDescription = "inputGroup-sizing-lg"
+    let myNodeList = document.querySelectorAll(`.td_sor${line}`)
+    for (let i = 0; i < myNodeList.length; i++) {
+        addInput.defaultValue = myNodeList[i].firstChild.nodeValue
+        myNodeList[i].appendChild(addInput.cloneNode(true))
+    }
 }
 btn_dele.onclick = function () {
-    alert('TÖRLÉS')
-    var td = document.querySelector("td")/*Miből*/
-    var remInput = document.querySelector(".form-control")/*Mit*/
-    td.removeChild(td)
+    const element = document.querySelector(`#C${line}`)
+    if (confirm("Biztos törölni akarod a rekordod?")) {
+        element.remove()
+        alert('Törlésre került!')
+    }else{
+        console.log('Siker!')
+    }
 }
