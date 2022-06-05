cc09b508ea4e2b588ee7395b545cb16e287c7b1d
NagypalMarton <nagypalmarton@outlook.com>
2022-06-05 18:32:26 +0200

    aa

--- js/adminbt_int.js
+++ js/adminbt_int.js
@@ -16,18 +16,13 @@ for (let i = 0; i < mytr.length; i++) {
     mytr[i].setAttribute("id", `C${i}`);
 }
 
-let btn_modi = document.querySelector("button[type='submit_mod']") /*Módosítás GOMB */
-let btn_dele = document.querySelector("button[type='submit_del']") /*Törlő GOMB */
-let btn_ment = document.querySelector("button[type='submit_ment']") /*Mentés GOMB */
-
-
 function choiceLine(line) {/*Aktuális sor sorszámát választja ki */
     let sorScope = document.querySelector('th[scope="row"]').firstChild.nodeValue
-    alert(sorScope)
+    alert(sorScope+'Nincs visszatérési érték!')
     return sorScope
 }
 
-btn_modi.onclick = function () {
+function btn_modi() {
     let addInput = document.createElement('input');
     line = choiceLine(line)
     addInput.className = "form-control td_sor"
@@ -37,9 +32,10 @@ btn_modi.onclick = function () {
         addInput.defaultValue = myNodeList[i].firstChild.nodeValue
         myNodeList[i].appendChild(addInput.cloneNode(true))
     }
-    document.querySelector('button[type="submit_mod"]').setAttribute('type','submit_ment')
+    btn_modi = document.querySelector('button[type="submit_mod"]').setAttribute('class', 'btn btn-success btn-group-s')/*Vajon lehet olyat, ami csak részlegesen cseréli? */
+    btn_modi = document.querySelector('button[type="submit_mod"]').setAttribute('type', 'submit_ment')
 }
-btn_dele.onclick = function () {
+function btn_dele() {
     line = choiceLine(line)
     const element = document.querySelector(`#C${line}`)
     if (confirm("Biztos törölni akarod a rekordod?")) {
@@ -50,15 +46,20 @@ btn_dele.onclick = function () {
     }
 }
 
-let table_row=document.querySelector(".table_row")
-table_row.addEventListener("submit_ment",function(ev){/*íGY MÁR el is küldhetnénk az adatokat */
+var btn_mentes=document.querySelector('button[type="submit_ment"]');
+btn_mentes.onclick=function(params) {
+    alert('Mentés')
+}
+
+let table_row = document.querySelector(".table_row")
+table_row.addEventListener("submit_ment", function (ev) {/*íGY MÁR el is küldhetnénk az adatokat */
     ev.preventDefault();
     /*console.log(this);*/
 
-    let inputs=this.querySelectorAll("input");
-    let values={}
+    let inputs = this.querySelectorAll("input");
+    let values = {}
     for (let i = 0; i < inputs.length; i++) {
-         values[inputs[i].name]=inputs.value
+        values[inputs[i].name] = inputs.value
     }
     console.log(values);
 });
