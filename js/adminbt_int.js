2fbca343e63168039210b7aeef1bb1f86e95f084
NagypalMarton <nagypalmarton@outlook.com>
2022-06-05 03:29:39 +0200

    9 - F2 partof

--- js/adminbt_int.js
+++ js/adminbt_int.js
@@ -1,10 +1,27 @@
-let tableForm = document.querySelector("#tableForm")
-tableForm.addEventListener("submit_mod", function (ev) {
-    ev.preventDefault();
-    console.log(this);
-    let tds = this.querySelectorAll("td");
-    let values = {};
-    for (let i = 0; i < tds.length; i++) {
-        values[tds[i].name] = tds[i].value;
+let mytd = document.querySelectorAll("td")
+let j=4
+for (let i = 0; i < mytd.length; i++) {
+    mytd[i].setAttribute("class", "td_sor");
+    if (i == j) {
+        mytd[i].setAttribute("class", "mod_gomb");
+        j+=5
     }
-});
+}
+
+let TableForm = document.querySelector(".table-group-divider")
+let btn_modi = document.querySelector("button[type='submit_mod']") /*Módosítás GOMB */
+let btn_dele = document.querySelector("button[type='submit_del'")
+btn_modi.onclick = function () {
+    alert('Teszt Szöveg')
+    let addInput = document.createElement("input");
+    addInput.className = "form-control"
+    let parents = document.querySelector("td")
+    parents.appendChild(addInput)
+    console.log(document.querySelector(".table_row").attributes)
+}
+btn_dele.onclick = function () {
+    alert('TÖRLÉS')
+    var td = document.querySelector("td")/*Miből*/
+    var remInput = document.querySelector(".form-control")/*Mit*/
+    td.removeChild(td)
+}
