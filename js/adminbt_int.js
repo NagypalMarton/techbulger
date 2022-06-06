3331a9a511cee205373b6a208b22f029b0ff5f97
NagypalMarton <nagypalmarton@outlook.com>
2022-06-06 17:23:12 +0200

    9 - F3 -4/5 (f2-esek nincs kijavítva)

--- js/adminbt_int.js
+++ js/adminbt_int.js
@@ -1,3 +1,85 @@
+let users = [
+    {
+        name: "Kiss Tibbi",
+        phone: "+36/90-965-9878",
+        email: "kiss.tibi@gags.ju",
+        address: '9876 Mucsaröcsöge Trabéz Nadrág utca 13'
+    },
+    {
+        name: "Nagy Józska",
+        phone: "+36/90-965-9878",
+        email: "Nagy.Jozska@gags.ju",
+        address: '9876 Mucsaröcsöge Trabéz Nadrág utca 19'
+    },
+    {
+        name: "Nől Zsófia",
+        phone: "+36/90-965-7899",
+        email: "Nol.Zsofia@gags.jh",
+        address: '9876 Mucsaröcsöge Trabéz Nadrág utca 9'
+    },
+]
+
+{/*
+<td>
+    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+        <div class="btn-group me-2" role="group" aria-label="First group">
+            <button onclick='btn_modi()' type="submit_mod"
+                class="btn btn-outline-warning btn-group-s" role="group"><i
+                    class="fa-solid fa-pen-nib"></i> Edit</button>
+            <button onclick='btn_dele()' type="submit_del"
+                class="btn btn-outline-danger btn-group-sm" role="group"><i
+                    class="fa-solid fa-dumpster-fire"></i> Del</button>
+        </div>
+    </div>
+</td> */}
+
+let tableBody = document.querySelector("#tableForm tbody");
+let createID = (html, parent) => {
+    let td = document.createElement("td")
+    td.innerHTML = html
+    parent.appendChild(td)
+}
+
+let createButtonGroup = parent => {
+    let toolbar = document.createElement("div")
+    toolbar.className = 'btn-toolbar'
+
+    let group = document.createElement("div")
+    group.className = "btn-group me-2";
+
+    let btnEdit = document.createElement("button");
+    btnEdit.className = "btn btn-outline-warning btn-group-s"
+    btnEdit.innerHTML = "<i class='fa-solid fa-pen-nib'></i> Edit"
+
+    let btnDel = document.createElement("button")
+    btnDel.className = "btn btn-outline-danger btn-group-sm"
+    btnDel.innerHTML = '<i class="fa-solid fa-dumpster-fire"></i> Del'
+
+    group.appendChild(btnEdit)
+    group.appendChild(btnDel)
+    toolbar.appendChild(group)
+
+    let td = document.createElement("td")
+    td.appendChild = (toolbar)/*group */
+    parent.appendChild(td)
+
+    document.querySelector('div[class="btn-toolbar"]').setAttribute('role','toolbar')/* Mit, Mire */
+    document.querySelector('div[class="btn-toolbar"]').setAttribute('aria-label','Toolbar with button groups')/* Mit, Mire */
+
+    document.querySelector('div[class*="btn-group me-2"]').setAttribute('role','group')/* Mit, Mire */
+    document.querySelector('div[class*="btn-group me-2"]').setAttribute('aria-label','First group')/* Mit, Mire */
+}
+
+for (const k in users) {
+    let tr = document.createElement("tr")
+    createID(parseInt(k) + 1, tr)
+    for (const value of Object.values(users[k])) {
+        createID(value, tr)
+    }
+    createButtonGroup(tr);
+    tableBody.appendChild(tr)
+}
+
 let mytd = document.querySelectorAll("td")
 let mytr = document.querySelectorAll("tr")
 let mod_osz = 4, line = 1
@@ -36,6 +118,7 @@ function btn_modi() {
     btn_modi = document.querySelector('button[type="submit_mod"]').setAttribute('onclick', 'btn_mentes()')
     btn_modi = document.querySelector('button[type="submit_mod"]').setAttribute('type', 'submit_ment')
 }
+
 function btn_dele() {
     line = choiceLine(line)
     const element = document.querySelector(`#C${line}`)
@@ -73,18 +156,6 @@ table_row.addEventListener("click", function (ev) {/*íGY MÁR el is küldhetné
     console.log(values);
 });
 
-/*Az adott fölé helyezett kurzor esetén változzon a line értéke */
-/*let buttonGrMoOv = document.querySelector("div[class*='btn-group']").onmouseover = function () { mouseOver() }
-let buttonGrMoOu = document.querySelector("div[class*='btn-group']").onmouseout = function () { mouseOut() }*/
-/*onmouseover: az elem fölé viszik az egérmutatót (mobilon értelmetlen) */
-
-/*function mouseOver() {
-    document.querySelector("div[class*='btn-group']").style.boxShadow = " 0 0 12px black"
-}
-function mouseOut() {
-    document.querySelector("div[class*='btn-group']").style.boxShadow = " 0 0 0px black"
-}*/
-
 let buttonGrMoOv = document.querySelectorAll("div[class*='btn-group']")
 let buttonGrMoOvs = document.querySelectorAll("div[class*='btn-group']")
 
@@ -97,80 +168,3 @@ buttonGrMoOvs.forEach(function (buttonGrMoOv) {
         document.querySelector("div[class*='btn-group']").style.boxShadow = " 0 0 0px black"
     });
 });
-
-let users = [
-    {
-        name: "Kiss Tibbi",
-        phone: "+36/90-965-9878",
-        email: "kiss.tibi@gags.ju",
-        address: '9876 Mucsaröcsöge Trabéz Nadrág utca 13'
-    },
-    {
-        name: "Nagy Józska",
-        phone: "+36/90-965-9878",
-        email: "Nagy.Jozska@gags.ju",
-        address: '9876 Mucsaröcsöge Trabéz Nadrág utca 19'
-    },
-    {
-        name: "Nől Zsófia",
-        phone: "+36/90-965-7899",
-        email: "Nol.Zsofia@gags.jh",
-        address: '9876 Mucsaröcsöge Trabéz Nadrág utca 9'
-    },
-]
-
-{/* <tr class="table_row">
-<th scope="row">1</th>
-<td>Mark Otto</td>
-<td>+36/20-123-4567</td>
-<td>mark.otto@thisemail.jk</td>
-<td>9876 Mucsaröcsöge Trabéz Nadrág utca 12.</td>
-<td>
-    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-        <div class="btn-group me-2" role="group" aria-label="First group">
-            <button onclick='btn_modi()' type="submit_mod"
-                class="btn btn-outline-warning btn-group-s" role="group"><i
-                    class="fa-solid fa-pen-nib"></i> Edit</button>
-            <button onclick='btn_dele()' type="submit_del"
-                class="btn btn-outline-danger btn-group-sm" role="group"><i
-                    class="fa-solid fa-dumpster-fire"></i> Del</button>
-        </div>
-    </div>
-</td>
-</tr> */}
-
-let tableBody = document.querySelector("#tableForm tbody");
-let createID = (html, parent) => {
-    let td = document.createElement("td")
-    td.innerHTML = html
-    parent.appendChild(td)
-}
-
-let createButtonGroup = parent => {
-    let group = document.createElement("div")
-    group.className = "btn-group";
-    let btnEdit = document.createElement("button");
-    btnEdit.className = "btn btn-outline-warning btn-group-s"
-    btnEdit.innerHTML = "<i class='fa-solid fa-pen-nib'></i> Edit"
-
-    let btnDel = document.createElement("btn")
-    btnDel.className = "btn btn-outline-danger btn-group-sm"
-    btnDel.innerHTML = '<i class="fa-solid fa-dumpster-fire"></i> Del'
-
-    group.appendChild(btnEdit)
-    group.appendChild(btnDel)
-
-    let td = document.createElement("td")
-    td.appendChild = (group)
-    parent.appendChild(td)
-}
-
-for (const k in users) {
-    let tr = document.createElement("tr")
-    createID(parseInt(k) + 1, tr)
-    for (const value of Object.values(users[k])) {
-        createID(value, tr)
-    }
-    createButtonGroup(tr)
-    tableBody.appendChild(tr)
-}
