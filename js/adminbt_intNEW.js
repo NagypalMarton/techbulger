e349a819222cd0e21545668fc6a5f4608cc9c6fa
NagypalMarton <nagypalmarton@outlook.com>
2022-07-18 03:27:06 +0200

    Update NEW 2 file

--- js/adminbt_intNEW.js
+++ js/adminbt_intNEW.js
@@ -1,21 +1,5 @@
-let btn_Edit = document.querySelector(".btn-primary")
-btn_Edit.onclick = function () {
-    alert('Szerkeztés!')
-    
-    let inputAdd=document.createElement("input")
-    
-    inputAdd.className="form-control"
-    inputAdd.type="text"
-    inputAdd.ariaLabel="default input example"
-
-    let line_parent=document.querySelector("#tr1:not(empty)")
-
-    let line_td=document.querySelectorAll("td")
-    for(let i=0;i<line_td.length;i++){
-        alert(i)
-        inputAdd.value=line_td[i].firstChild.nodeValue
-        line_parent.removeChild(line_td[i])
-        line_parent.appendChild(line_parent.appendChild(inputAdd))
-        //alert(line_td[i].firstChild.nodeValue)
-    }
-}
+let line_Form = document.querySelector('#tr1')
+line_Form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    console.log(this);
+});
