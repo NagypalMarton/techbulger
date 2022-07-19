0066c8a1852739549fd962251bf8fdc3aa39ca0a
NagypalMarton <nagypalmarton@outlook.com>
2022-07-19 19:25:17 +0200

    M9 - F2 /Rész Megoldás/

--- js/adminbt_intNEW.js
+++ js/adminbt_intNEW.js
@@ -1,17 +1,17 @@
 let line_Form = document.querySelectorAll('tbody tr')
 for (const line_TR of line_Form) {
-    line_TR.addEventListener("click", function (ev) {
+    line_TR.addEventListener("click", function (ev) { //Edit gomb MEGnyomásával kellene müködnie
         ev.preventDefault();//Meg lehet így előzni az alapértelmezett lefutást
         console.log(this);
-
-        let inputAdd = document.createElement("input")
-        inputAdd.className = "form-control"
-        inputAdd.ariaLabel = "default input example"
 
         let lFtd = line_TR.querySelectorAll("td")
         console.log(lFtd)
 
         for (let i = 0; i < lFtd.length - 1; i++) {
+            let inputAdd = document.createElement("input")
+            inputAdd.className = "form-control"
+            inputAdd.ariaLabel = "default input example"
+
             inputAdd.value = lFtd[i].firstChild.data
             inputAdd.defaultValue=inputAdd.value
             console.log(lFtd[i].firstChild.data)
