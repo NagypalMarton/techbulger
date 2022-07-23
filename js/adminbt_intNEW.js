d7c6bccec03094ed8993d7a6d898218592ede801
NagypalMarton <nagypalmarton@outlook.com>
2022-07-23 16:25:24 +0200

    M9 - F2 félkész

--- js/adminbt_intNEW.js
+++ js/adminbt_intNEW.js
@@ -4,22 +4,6 @@ for (const line_TR of line_Form) {
         ev.preventDefault();//Meg lehet így előzni az alapértelmezett lefutást
         console.log(this);
 
-        let lFtd = line_TR.querySelectorAll("td")
-        console.log(lFtd)
-
-        for (let i = 0; i < lFtd.length - 1; i++) {
-            let inputAdd = document.createElement("input")
-            inputAdd.className = "form-control"
-            inputAdd.ariaLabel = "default input example"
-
-            inputAdd.value = lFtd[i].firstChild.data
-            inputAdd.defaultValue=inputAdd.value
-            console.log(lFtd[i].firstChild.data)
-            lFtd[i].appendChild(inputAdd)
-            lFtd[i].removeChild(lFtd[i].firstChild)
-            console.log(lFtd[i])
-        }
-
         let inputs = this.querySelectorAll("input")
         let values = {}
         for (let i = 0; i < inputs.length; i++) {
@@ -28,27 +12,47 @@ for (const line_TR of line_Form) {
         console.log(values)
     });
 }*/
-let dedi_line
-//A függvényt hívó gomb meghatározásához kell => LEHET, HOGY A CELLÁKNÁL IS MEGHÍVÓDIK!!
-let active_btns=document.querySelectorAll('button')
-for(const active_btn of active_btns){
-    active_btn.addEventListener("click",function(ev) {
+
+let edit_btns = document.querySelectorAll('[onclick*=btn_Edit]')//Módosító gomb valós
+for (const edit_btn of edit_btns) {
+    edit_btn.addEventListener("click", function (ev) {
         ev.preventDefault();//Meg lehet így előzni az alapértelmezett lefutást
-        console.log(this);
-        dedi_line=this.parentElement.parentElement.parentElement
-        console.log(dedi_line)
+        let dedi_line = this.parentElement.parentElement.parentElement
+
+        let lFtd = dedi_line.querySelectorAll("td")
+
+        for (let i = 0; i < lFtd.length - 1; i++) {
+            let inputAdd = document.createElement("input")
+            inputAdd.className = "form-control"
+            inputAdd.ariaLabel = "default input example"
+
+            inputAdd.value = lFtd[i].firstChild.data
+            inputAdd.defaultValue = inputAdd.value
+            lFtd[i].appendChild(inputAdd)
+            lFtd[i].removeChild(lFtd[i].firstChild)
+        }
+        //gomb csere Mentés gombra
+        let saveIconAdd = document.createElement("i")
+        saveIconAdd.className = "fa-solid fa-floppy-disk"
+        let saveButtonAdd = document.createElement("button")
+        saveButtonAdd.className = "btn btn-warning"
+        saveButtonAdd.type = "button"
+        saveButtonAdd.textContent = 'Save'
+        //saveButtonAdd.onclick =btn_Save() //function () { alert('teszt') }
+        this.parentElement.replaceChild(saveButtonAdd, this)
+        saveButtonAdd.appendChild(saveIconAdd)
+        saveButtonAdd.setAttribute(onclick)
     })
 }
 
 function btn_Edit() { //Módosító gomb => Feldat: TD text gyereke helyére input mezőt tegyen a gyerek értékével (Űrlap lesz a vége), majd váltson át Mentés gombra
     alert('Szerkeztés!')
-    console.log('btn_editMa: '+dedi_line)
-    let active_btn_line=document.querySelector('button:active')
-    console.log('active_btn_line => '+active_btn_line) //nem müködik
-    console.log(btn_Edit.caller.name)
+}
+
+function btn_Save(params) {
+    alert('Save!')
 }
 
 function btn_Del() { //Törlő gomb =>  Feldat: Törölje a sort
     alert('Törlés!')
-    console.log('btn_DELMa: '+dedi_line)
 }
