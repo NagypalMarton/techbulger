6b4e22ce61e701c4e55064621eac425f6a4b1b8f
NagypalMarton <nagypalmarton@outlook.com>
2022-07-25 04:30:59 +0200

    M9 - FULL COMPLETE

--- js/adminbt_intNEW.js
+++ js/adminbt_intNEW.js
@@ -23,60 +23,96 @@ let users = [
     { name: "Kiss Bence", telNumb: "+36/03-166-4567", email: "kissbence@email.com", address: "1234 Mucsaröcsöge Bobó körút 126" }
 ]
 
-//DOM manipuláció for ciklussal
-let tableBody = document.querySelector("#userTable tbody")
-let createTD=(html,parent)=>{
-    let td = document.createElement("td")
-    td.innerHTML = html
-    parent.appendChild(td)
-}
+function createTable() {
+    //DOM manipuláció for ciklussal
+    let tableBody = document.querySelector("#userTable tbody")
 
-//Gombok elkészítésének refaktorálása
-let createButtonGroup=parent=>{
-    let group=document.createElement("div")
-    group.className="btn-group"
-    group.ariaLabel="Basic example"
-
-    let btnInfo=document.createElement("button")
-    btnInfo.className="btn btn-primary"
-    btnInfo.innerHTML="<i class='fa-solid fa-pen-nib'></i>EDIT"
-
-    let btnDanger=document.createElement("button")
-    btnDanger.className="btn btn-danger"
-    btnDanger.innerHTML="<i class='fa-solid fa-eraser'></i>DEL"
-
-    group.appendChild(btnInfo)
-    group.appendChild(btnDanger)
-
-    let td = document.createElement("td")
-    td.appendChild(group)
-    parent.appendChild(td)
-    group.setAttribute("role","group")
-    btnInfo.setAttribute("onclick","btn_Edit()")
-    btnDanger.setAttribute("onclick","btn_Del()")
-}
-//vége
-
-for (let k in users) {
-    let tr = document.createElement("tr")
-    let th = document.createElement("th")
-    th.innerHTML = parseInt(k) + 1
-    tr.appendChild(th)
-    tr.id=`tr${(parseInt(k)+1)}`
-    for(let value of Object.values(users[k])){
-        createTD(value,tr)
+    //Korábbi táblázat soraink a törlése, eltávolítása
+    let trs = tableBody.querySelectorAll("tr")
+    if (tableBody.childElementCount > 0) {
+        for (let i = 0; i < trs.length; i++) {
+            tableBody.removeChild(trs[i])
+        }
     }
-    createButtonGroup(tr)
-    tableBody.appendChild(tr)
+
+    let createTD = (html, parent) => {
+        let td = document.createElement("td")
+        td.innerHTML = html
+        parent.appendChild(td)
+    }
+
+    //Gombok elkészítésének refaktorálása
+    let createButtonGroup = parent => {
+        let group = document.createElement("div")
+        group.className = "btn-group"
+        group.ariaLabel = "Basic example"
+
+        let btnInfo = document.createElement("button")
+        btnInfo.className = "btn btn-primary"
+        btnInfo.innerHTML = "<i class='fa-solid fa-pen-nib'></i>EDIT"
+
+        let btnDanger = document.createElement("button")
+        btnDanger.className = "btn btn-danger"
+        btnDanger.innerHTML = "<i class='fa-solid fa-eraser'></i>DEL"
+
+        group.appendChild(btnInfo)
+        group.appendChild(btnDanger)
+
+        let td = document.createElement("td")
+        td.appendChild(group)
+        parent.appendChild(td)
+        group.setAttribute("role", "group")
+        btnInfo.setAttribute("onclick", "btn_Edit()")
+        btnDanger.setAttribute("onclick", "btn_Del()")
+    }
+    //vége
+
+    for (let k in users) {
+        let tr = document.createElement("tr")
+        let th = document.createElement("th")
+        th.innerHTML = parseInt(k) + 1
+        tr.appendChild(th)
+        tr.id = `tr${(parseInt(k) + 1)}`
+        for (let value of Object.values(users[k])) {
+            createTD(value, tr)
+        }
+        createButtonGroup(tr)
+        tableBody.appendChild(tr)
+    }
 }
 
-function btn_Edit(button) { //Módosító gomb => Feldat: TD text gyereke helyére input mezőt tegyen a gyerek értékével (Űrlap lesz a vége), majd váltson át Mentés gombra
-    alert('Edit! Az adott sorban 2. kattintás után nem müködik az AddEvenListener() valamiért!')
-    console.log(button.target)
+createTable()
+
+let edit_btns = document.querySelectorAll('[onclick*=btn_Edit]')//Módosító gomb valós
+
+function btn_Edit() { //Módosító gomb => Feldat: TD text gyereke helyére input mezőt tegyen a gyerek értékével (Űrlap lesz a vége), majd váltson át Mentés gombra
+    let aktBtn = document.querySelector("[onclick*=btn_Edit]:hover")
+    let lFtd = aktBtn.parentElement.parentElement.parentElement.querySelectorAll("td")
+
+    for (let i = 0; i < lFtd.length - 1; i++) {
+        let inputAdd = document.createElement("input")
+        inputAdd.className = "form-control"
+        inputAdd.ariaLabel = "default input example"
+        inputAdd.value = lFtd[i].firstChild.data
+        inputAdd.defaultValue = inputAdd.value
+        lFtd[i].appendChild(inputAdd)
+        lFtd[i].removeChild(lFtd[i].firstChild)
+    }
+    //EDIT gomb csere SAVE gombra
+    let saveIconAdd = document.createElement("i")
+    saveIconAdd.className = "fa-solid fa-floppy-disk"
+    let saveButtonAdd = document.createElement("button")
+    saveButtonAdd.className = "btn btn-warning"
+    saveButtonAdd.type = "button"
+    saveButtonAdd.textContent = 'Save'
+    aktBtn.parentElement.replaceChild(saveButtonAdd, aktBtn)
+    saveButtonAdd.appendChild(saveIconAdd)
+    saveButtonAdd.setAttribute('onclick', 'btn_Save()')
+    edit_btns = document.querySelectorAll('[onclick*=btn_Edit]')
+    line_Form = document.querySelector('[class="btn btn-warning"]')
 }
 
 function btn_Save() {//Mentés sor: Adott sorban évő adatokat menti
-    alert('Save!')
     let inputs = line_Form.parentElement.parentElement.parentElement.querySelectorAll("input")
     let input_TdText = line_Form.parentElement.parentElement.parentElement.querySelectorAll("td")
     let values = {}
@@ -95,6 +131,8 @@ function btn_Save() {//Mentés sor: Adott sorban évő adatokat menti
     line_Form.parentElement.replaceChild(saveButtonAdd, line_Form)
     saveButtonAdd.appendChild(EditIconAdd)
     saveButtonAdd.setAttribute('onclick', 'btn_Edit()')
+    edit_btns = document.querySelectorAll('[onclick*=btn_Edit]')
+    console.log(edit_btns)
 }
 
 function btn_Del() { //Törlő gomb =>  Feldat: Törölje az adott sort
@@ -103,51 +141,27 @@ function btn_Del() { //Törlő gomb =>  Feldat: Törölje az adott sort
 }
 
 function sendButton() {
-    alert('SEND button')
-}
-
-let edit_btns = document.querySelectorAll('[onclick*=btn_Edit]')//Módosító gomb valós
-let line_Form //Save Gombhoz kell
-for (const edit_btn of edit_btns) {
-  //  console.log("aEL-en kivül és előtt")
-    edit_btn.addEventListener("click", function (ev) {
-        ev.preventDefault();//Meg lehet így előzni az alapértelmezett lefutást
-        let lFtd = this.parentElement.parentElement.parentElement.querySelectorAll("td")
-    //  console.log("aEL-ben")
-
-        for (let i = 0; i < lFtd.length - 1; i++) {
-            let inputAdd = document.createElement("input")
-            inputAdd.className = "form-control"
-            inputAdd.ariaLabel = "default input example"
-            inputAdd.value = lFtd[i].firstChild.data
-            inputAdd.defaultValue = inputAdd.value
-            lFtd[i].appendChild(inputAdd)
-            lFtd[i].removeChild(lFtd[i].firstChild)
-        }
-        //EDIT gomb csere SAVE gombra
-        let saveIconAdd = document.createElement("i")
-        saveIconAdd.className = "fa-solid fa-floppy-disk"
-        let saveButtonAdd = document.createElement("button")
-        saveButtonAdd.className = "btn btn-warning"
-        saveButtonAdd.type = "button"
-        saveButtonAdd.textContent = 'Save'
-        this.parentElement.replaceChild(saveButtonAdd, this)
-        saveButtonAdd.appendChild(saveIconAdd)
-        saveButtonAdd.setAttribute('onclick', 'btn_Save()')
-        edit_btns = document.querySelectorAll('[onclick*=btn_Edit]')
-        line_Form = document.querySelector('[class="btn btn-warning"]')
-    })
+    alert('A funkció addEventListenet-rel van megoldva!')
 }
 
 //űrlapesemény
-let userForm=document.querySelector('#userForm')
-userForm.addEventListener("submit",function(ev){
+let userForm = document.querySelector('#userForm')
+userForm.addEventListener("submit", function (ev) {
     ev.preventDefault()
 
-    let inputs=this.querySelectorAll("input")
-    let values={}
+    let inputs = this.querySelectorAll("input")
+    let values = {}
     for (let i = 0; i < inputs.length; i++) {
-        values[inputs[i].name]=inputs[i].value 
+        values[inputs[i].name] = inputs[i].value
+        inputs[i].value = ""
     }
-    console.log(values)
+
+    users.push({
+        name: `${values.nev}`,
+        telNumb: `${values.telSzam}`,
+        address: `${values.email}`,
+        email: `${values.address}`
+    })
+
+    createTable()
 })
